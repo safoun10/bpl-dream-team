fb9d676779a42c5e24594087148f90a3d4af96ae
Safoun <safoun.official@gmail.com>
2026-09-16 16:27:28 +0600

    testing out react+ts+tw+daisy stuffs

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+    return (
+        <div>
+            <div className="text-5xl">test</div>
+            <button className="btn btn-neutral">Neutral</button>
+        </div>
+    )
+}
+
+export default App
